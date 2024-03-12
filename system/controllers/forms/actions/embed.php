9ddd37b4686376c48744ad90dd2b560a00400925
ВСообществе <84793732+rutubesoft@users.noreply.github.com>
2024-03-12 21:10:48 +0600

    init

---
+++ system/controllers/forms/actions/embed.php
@@ -0,0 +1,36 @@
+<?php
+
+class actionFormsEmbed extends cmsAction {
+
+    public function run($hash){
+
+        if(!$this->isAllowEmbed() || is_numeric($hash)){
+            cmsCore::error404();
+        }
+
+        $_form_data = $this->getFormData($hash);
+
+        if($_form_data === false){
+            return cmsCore::error404();
+        }
+
+        // Здесь виджеты не нужны
+        $this->cms_template->widgets_rendered = true;
+
+        $this->cms_template->setLayout('controllers/forms/embed_layout');
+
+        list($form, $form_data) = $_form_data;
+
+        $submited_data = $this->getSavedUserFormData($form_data['id']);
+
+        if($submited_data && !empty($form_data['options']['hide_after_submit'])){
+            $this->halt();
+        }
+
+        return $this->cms_template->render('form_view', [
+            'form_data' => $form_data,
+            'form'      => $form
+        ]);
+    }
+
+}
